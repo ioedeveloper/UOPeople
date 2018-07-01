4d4b27667d4777fe22b843955b7b2c255f9a33ac
ioedeveloper <ioedeveloper@DESKTOP-113GA6A>
2018-07-01 13:05:05 +0100

    Assignment1

---
+++ Assignment1/t_area.py
@@ -0,0 +1,12 @@
+# assign the base value
+base = 12
+
+# assign the height value
+height = 16
+
+# assign the results of the operation to the variable area 
+# the operation half base by height
+area = 1/2 * base * height
+
+# print the value of area 
+print(area)
