15fc04def39dfbdff2f212ffa35cc14255c02b14
ioedeveloper <ioedeveloper@DESKTOP-113GA6A>
2018-07-17 20:43:06 +0100

    Assignment2

--- Assignment2/tryme3.py
+++ Assignment2/tryme3.py
@@ -1,16 +1,20 @@
+# function that prints a single line
 def new_line():
    print()
 
+# function that prints three lines by calling new_line() function
 def three_lines():
    new_line()
    new_line()
    new_line()
 
+# function that prints nine lines by calling three_lines() function
 def nine_lines():
     three_lines()
     three_lines()
     three_lines()
 
+# function that prints 25 lines by calling 2 nine_lines(), 2 three_lines() and a new_line() function
 def clear_screen():
     nine_lines()
     nine_lines()
@@ -18,6 +22,7 @@ def clear_screen():
     three_lines()
     new_line()
 
+# function call to clear_screen() function
 clear_screen()
     
     
